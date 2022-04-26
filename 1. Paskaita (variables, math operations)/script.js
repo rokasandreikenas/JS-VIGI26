ec139134fb1288a0bf9f5b0ad31863b940e7d66a
“rokasandreikenas” <shyptt@gmail.com>
2022-04-26 20:35:28 +0300

    Add js file

---
+++ 1. Paskaita (variables, math operations)/script.js
@@ -0,0 +1,14 @@
+// One line komentaras
+
+/* 
+    Sveiki čia yra
+    Multi line komentaras
+*/
+
+// Sukuriam kintamuosius
+let myName;
+let age;
+
+// Priskiriam kintamiejam reikšmes
+myName = "Rokas";
+age = 23;
