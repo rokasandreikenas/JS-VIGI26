f14619f4852fa8843c2620016ac5db80c31070ca
Rokas Andreikenas <rokas.andreikenas@exacaster.com>
2022-05-03 19:55:47 +0300

    for, do, do while

---
+++ 4_paskaita/script.js
@@ -0,0 +1,92 @@
+// console.log("Ketvirta paskaita, for ciklai");
+
+// i++ prideda 1
+// i = i + 1;
+
+// i-- atima 1
+// i = i - 1
+
+// +=   x += y   x = x + y
+// -=   x -= y   x = x - y
+
+// kintamieji turi būti i, j, k
+// [inicializavimas];[sąlyga];[išraiška]
+// for (let i = 0; i < 10; i++) {
+//   console.log(`${i + 1}. Rokas`);
+// }
+
+// const vardas = "Rokas";
+// const count = 10;
+
+// for (let i = 0; i < count; i++) {
+//   console.log(`${i + 1}. ${vardas}`);
+// }
+
+// let i = 10;
+// while (i > 0) {
+//   console.log(i);
+//   i--;
+// }
+
+// for (let i = 10; true; i--) {
+//   console.log(i);
+// }
+
+// for (let i = 4; i < 11; i += 1.5) {
+//   console.log(i);
+// }
+// i = 0; 0 < 10 ; 0 = 0 + 2
+// i = 2; 2 < 10 ; 2 = 2 + 2
+// i = 4; 4 < 10 ; 4 = 4 + 2
+// console.log(" ");
+// for (let i = 0; i < 10; i += 3) {
+//   console.log(i);
+// }
+
+// const dogName = "Rikis";
+// const repeat = 5;
+// let result = "";
+
+// for (let i = 0; i < repeat; i++) {
+//   if (i === repeat - 1) {
+//     // paskutinis ciklas
+//     result += `${dogName}.`;
+//   } else {
+//     result += `${dogName}, `;
+//   }
+// }
+
+// a = a + b
+// "" = "" + "Rikis"
+// "Rikis" = "Rikis" + "Rikis"
+// "RikisRikis" = "RikisRikis" + "Rikis"
+// "RikisRikisRikis" = "RikisRikisRikis" + "Rikis"
+// "RikisRikisRikisRikis" = "RikisRikisRikisRikis" + "Rikis"
+
+// const start = 1;
+// const count = 9;
+// let result = 0;
+
+// for (let i = start; i < count + 1; i++) {
+//   result += i;
+// }
+
+// console.log(result);
+
+// inicializavimas
+// let i = 0;
+// // salyga
+// while (i < 10) {
+//   // shows 0, then 1, then 2
+//   console.log(i);
+//   // išraišką
+//   i++;
+// }
+
+// do...while ciklas pirmiausia įvykdys kūną, tada patikrins išraišką ir, kol tai tiesa, vėl ir vėl ją vykdys.
+
+// let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 3);
