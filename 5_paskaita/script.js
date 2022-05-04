cb9cc1498de64ea09ed6b8b6a748c7b0dfc736b8
Rokas Andreikenas <rokas.andreikenas@exacaster.com>
2022-05-04 20:13:34 +0300

    Add dom examples

---
+++ 5_paskaita/script.js
@@ -0,0 +1,23 @@
+const nameNode = document.getElementById("name");
+nameNode.textContent = "Tomas";
+
+const descriptionNode = document.getElementById("description");
+descriptionNode.textContent = "Man patinka programuoti";
+
+document.body.style.backgroundColor = "#f1f1f1";
+document.body.style.margin = 0;
+nameNode.style.color = "blue";
+nameNode.style.paddingLeft = "10px";
+descriptionNode.style.padding = "10px";
+descriptionNode.style.backgroundColor = "cyan";
+
+// const liByTag = document.querySelector("li");
+// const liById = document.querySelector("#discount");
+// const liByClass = document.querySelector(".yellow");
+
+// const firstItem = document.querySelector("li:first-child");
+// const lastItem = document.querySelector("li:last-child");
+
+// const discountedItems = document.querySelectorAll("#discount");
+// console.log(discountedItems[0]);
+// console.log(discountedItems.length);
