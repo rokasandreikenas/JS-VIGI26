e6686ec5cf0da3e6edfbf525a503b02dd5c4716d
Rokas Andreikenas <rokas.andreikenas@exacaster.com>
2022-06-28 20:17:25 +0300

    Add delete button with DELETE method

--- 25_paskaita/script.js
+++ 25_paskaita/script.js
@@ -1,15 +1,20 @@
 const renderTodo = (todo) => {
-  const { completed, title } = todo;
+  const { id, completed, title } = todo;
 
   const todoEl = document.createElement("div");
   const todoTitle = document.createElement("p");
+  const todoActions = document.createElement("div");
   const todoStatus = document.createElement("span");
+  const deleteButton = document.createElement("button");
 
   todoEl.className = "todo";
   todoTitle.className = completed ? "todo-title done" : "todo-title";
   todoStatus.className = completed ? "todo-status done" : "todo-status";
+  todoActions.className = "todo-actions";
+  deleteButton.className = "todo-delete";
 
   todoTitle.textContent = title;
+  deleteButton.textContent = "Delete";
 
   todoStatus.addEventListener("click", () => {
     todoStatus.classList.toggle("done");
@@ -17,7 +22,23 @@ const renderTodo = (todo) => {
     todoStatus.completed = !todoStatus.completed;
   });
 
-  todoEl.append(todoTitle, todoStatus);
+  deleteButton.addEventListener("click", () => {
+    const params = {
+      method: "DELETE",
+    };
+
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, params)
+      .then((resp) => resp.json())
+      .then(() => {
+        console.log("successfully deleted: ", todo);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+
+  todoActions.append(todoStatus, deleteButton);
+  todoEl.append(todoTitle, todoActions);
   document.querySelector(".todo-container").prepend(todoEl);
 };
 
@@ -31,11 +52,6 @@ fetch("https://jsonplaceholder.typicode.com/todos")
   .catch((error) => {
     console.error(error);
   });
-
-// issitraukti mygtuka +
-// priskirti addEvent listener +
-// issitraukti funkcijos viduje input lauka +
-// input duomenis append i todo lista
 
 const handleAddTodo = () => {
   const todoText = document.querySelector('input[name="todo-text"]');
@@ -62,9 +78,3 @@ const handleAddTodo = () => {
 
 const addButton = document.querySelector(".add-button");
 addButton.addEventListener("click", handleAddTodo);
-
-// Sukurti addEventListener su handlerliu onClick
-// Nuspaudus ant elemento pakeisti todoStatus klasę (jeigu klasė yra done, pakeisti į undone. Jeigu klasė yra undone, pakeisti į done)
-// Pakeisti todo status į priešinga, kaip su klase. jeigu buvo todo.completed = true, pakeisti i todo.completed = false;
-// Pakeisti teksto sviesuma/tamsuma kai yra done/undone
-// Follow dizaina, datas: menesis, diena, metai
